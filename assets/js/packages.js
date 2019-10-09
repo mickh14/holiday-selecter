3eb50362fa080b78334bb3e46732cb64cb302fc0
EC2 Default User <ec2-user@ip-172-31-91-116.ec2.internal>
2019-10-09 15:21:42 +0000

    update places autom-complete and map address location

--- assets/js/packages.js
+++ assets/js/packages.js
@@ -1,36 +1,50 @@
-function fetchorigin() {
-     var origin = document.getElementById("origin").value;
-     console.log(origin);
-     }
 
-function fetchdest() {
-     var dest = document.getElementById("dest").value;
-     console.log(dest);
-     }
-
-function fetchtype() {
-     var type = document.getElementById("type").value;
-     console.log(type);
-     }
-
-/*$(document).ready(function() {
-    $.get('rompack.html', function(data) {
-        $('#romantic-packages').html(data);
-    });
-});*/
-
-
-if(dest ="paris"){
-   $(document).ready(function() {
-    $.get('rompack.html', function(data) {
-        $('#romantic-packages').html(data);
-    });
-});
-} else if (dest ="berlin"){
-    $(document).ready(function() {
-    $.get('fampack.html', function(data) {
-        $('#family-packages').html(data);
-    });
-});
+/*-----------------Autocomplete locations
+function activatePlacesSearch() {
+   var input = document.getElementById('search_term');
+   var Autocomplete = new google.maps.places.Autocomplete(input);
 }
+------------------works when intit map switched off --------------------------/
 
+/*-----------------Autocomplete function------------------------*/
+$('#search_term').on('click',function(){
+    
+    // Create the search box and link it to the UI element.
+    //basic google map api places for search bar
+    var input = document.getElementById('search_term');
+   var Autocomplete = new google.maps.places.Autocomplete(input);
+   // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+   
+});
+
+
+$('#whereTo').on('click',function(){
+    
+    // Create the search box and link it to the UI element.
+    //basic google map api places for search bar
+    var enter = document.getElementById('whereTo');
+   var Autocomplete = new google.maps.places.Autocomplete(enter);
+   // map.controls[google.maps.ControlPosition.TOP_LEFT].push(enter);
+   
+});
+
+/*-----------------Fetch and display packages to match search criteria*/
+
+
+
+
+
+function fetchhotels(event) {
+
+
+
+    var username = $("#city").val();
+
+    if (!username) {
+
+        $("#hotelpic").html(`<h2>Please enter search criteria</h2>`);
+
+        return;
+
+    }
+}
