f5fc2318b6cdd327704041b4b091449199b36f97
EC2 Default User <ec2-user@ip-172-31-91-116.ec2.internal>
2019-10-15 13:42:35 +0000

    Add date fields JS and hotel API connection

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,9 +4,8 @@ var infowindow;
 
 
 
-
 function initMap() {
-  var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: pyrmont,
@@ -28,7 +27,7 @@ function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
-      console.log(place);
+      //console.log(place);
       createMarker(place);
     }
   }
@@ -41,17 +40,8 @@ function createMarker(place) {
             <h1 id="firstHeading" class="firstHeading">${place.name}</h1>
             <div id="bodyContent">
             <img src="${place.icon}"/>
-            <p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large 
-            sandstone rock formation in the southern part of the '+
-            Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) 
-            south west of the nearest large town, Alice Springs; 450&#160;km 
-            (280&#160;mi) by road. Kata Tjuta and Uluru are the two major 
-            features of the Uluru - Kata Tjuta National Park. Uluru is 
-            sacred to the Pitjantjatjara and Yankunytjatjara, the 
-            Aboriginal people of the area. It has many springs, waterholes, 
-            rock caves and ancient paintings. Uluru is listed as a World 
-            Heritage Site.</p>
-            <p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
+            <p><b>${place.name}</b>, is located in <b>${place.vicinity}</b></p>
+            <p>Attribution: ${place.name}, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
             https://en.wikipedia.org/w/index.php?title=Uluru</a> 
             (last visited June 22, 2009).</p>
             </div>
@@ -70,8 +60,114 @@ function createMarker(place) {
 
 }
 
+document.getElementById("find").addEventListener("click", function(){
+  //clearLocations();
+  // grab user input values
+  var searchTerm = document.getElementById("searchTerm").value;
+  var address = document.getElementById("whereTo").value;
+  //console.log(searchTerm);
 
+  //TODO geocode address
+  // call geocoder passing in address from user input #location
+  geoCodeAddress(address)
+  // geocoder returns a "then-able" promise with results
+  // .then only runs after the promise resolves
+  .then(function(results) {
+    // when geocoder is done log the results in console
+    updateMap(results[0].geometry.location, searchTerm);
+    console.log(results);
+  });
+});
+
+function geoCodeAddress(address) {
+  // return a Promise
+  var geocoder = new google.maps.Geocoder();
+  return new Promise(function(resolve,reject) {
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        // resolve results upon a successful status
+        resolve(results);
+      } else {
+        // reject status upon un-successful status
+        reject(status);
+      }
+    });
+  });
+}
+
+
+function updateMap(location, searchTerm) {
+  var updateCenter = location;
+  
+  //create the map  
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: updateCenter,
+    zoom: 15
+  });
+    infowindow = new google.maps.InfoWindow();
+
+  var request = {
+    location: updateCenter,
+    radius: '500',
+    type: searchTerm
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      //console.log(place);
+      createMarker(place);
+    }
+  }
+}
 /*================================================
+
+updateMap(results[0].geometry.location);
+
+
+
+/get the GeoCode details
+function geoCodeAddress(address) {
+  // return a Promise
+  return new Promise(function(resolve, reject) {
+    geocoder.geocode({ 'address': address }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        // resolve results upon a successful status
+        resolve(results);
+      }
+      else {
+        // reject status upon un-successful status
+        reject(status);
+      }
+    });
+  });
+}
+
+
+
+
+// findDetail() takes in a place object
+function findDetail(place) {
+  // return promise
+  return new Promise(function(resolve, reject) {
+    // use getDetails method to retrieve Place data via the Place's place_id property
+    service.getDetails({ placeId: place.place_id }, function(place, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        // upon successful request resolve place
+        resolve(place);
+      }
+      else {
+        // else reject with status
+        reject(status);
+      }
+    });
+  });
+
 
 function updateMap(location) {
   //new map coords
@@ -199,29 +295,7 @@ function geoCodeAddress(address) {
   });
 }
 
-// nearbySearch() needs a LatLng location, radius, and user's search term.
-function nearbySearch(location,rad, searchTerm) {
-  // setup request object
-  var request = {
-    // location is a LatLng object
-    location: location,
-    radius: rad,
-    name: searchTerm
-  };
-  // return promise
-  return new Promise(function(resolve, reject) {
-    service.nearbySearch(request, function(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        // resolve promise with results on OK status
-        resolve(results);
-      }
-      else {
-        // reject promise otherwise
-        reject(status);
-      }
-    });
-  });
-}
+
 
 
 // findDetail() takes in a place object
--- assets/js/packages.js
+++ assets/js/packages.js
@@ -27,113 +27,129 @@ $('#whereTo').on('click', function() {
 
 });
 
-/*-----------------Fetch and display packages using Ajax------------------------------*/
+
+
+
+/*-----------------Date Picker------------------------------*/
+$(function() {
+    $('.dates #depDate').datepicker({
+        'format': 'yyyy-mm-dd',
+        'autoclose': false
+    });
+});
+
+$(function() {
+    $('.dates #arrDate').datepicker({
+        'format': 'yyyy-mm-dd',
+        'autoclose': false
+    });
+});
+
+
+
+/*-----------------Fetch and display packages using Ajax------------------------------
 
 function fetchtype() {
 
     var selectType = document.getElementById("holType").value;
     var selectdest = document.getElementById("search_term").value;
     console.log(selectdest);
-    $('#locpic').append($("selectdest.value"));
+    $('#locpic').
 
 }
-
+*/
 
 /*-----------------booking apix------------------------------
-var req = unirest("GET", "https://apidojo-booking-v1.p.rapidapi.com/properties/list");
-
-req.query({
-	"price_filter_currencycode": "USD",
-	"travel_purpose": "leisure",
-	"categories_filter": "price::9-40,free_cancellation::1,class::1,class::0,class::2",
-	"search_id": "none",
-	"order_by": "popularity",
-	"children_qty": "2",
-	"languagecode": "en-us",
-	"children_age": "5,7",
-	"search_type": "city",
-	"offset": "0",
-	"dest_ids": "-3712125",
-	"guest_qty": "1",
-	"arrival_date": "2019-03-13",
-	"departure_date": "2019-03-15",
-	"room_qty": "1"
-});
-
-req.headers({
-	"x-rapidapi-host": "apidojo-booking-v1.p.rapidapi.com",
-	"x-rapidapi-key": "ecace6c59emsh92e31436031b0c6p1b84d8jsnca11e04da872"
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
-===================================
+https://apidojo-booking-v1.p.rapidapi.com/properties/list?arrival_date=+arrDate+&departure_date=2019-11-15&price_filter_currencycode=USD&travel_purpose=leisure&categories_filter=price%3A%3A9-40%2Cfree_cancellation%3A%3A1%2Cclass%3A%3A1%2Cclass%3A%3A0%2Cclass%3A%3A2&search_id=none&order_by=popularity&children_qty=2&languagecode=en-us&children_age=5%2C7&search_type=city&offset=0&dest_ids=-3712125&guest_qty=1&room_qty=1
 
 */
 
-/*
-$.ajax({
-    method: 'GET',
-    url: 'https://jsonplaceholder.typicode.com/posts',
-    dataType: 'json'
-}).done(function(data) {
-    console.log(data);
-    $.map(data, function(post, i) {
-        $('#packagepref').append('<h3>' + post.title + '</h3><p>' + post.body + '</p>');
-    });
-});
 
+function fethHotelDetails() {
+    var arrDate = $("#enterDate").val();
+    //var depDate = $("#depDate").val();
+    console.log(arrDate);
+    
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://apidojo-booking-v1.p.rapidapi.com/properties/list?arrival_date=2019-11-15&departure_date=2019-11-19&dest_ids=-3712125",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "apidojo-booking-v1.p.rapidapi.com",
+		"x-rapidapi-key": "ecace6c59emsh92e31436031b0c6p1b84d8jsnca11e04da872"
+	}
+};
 
 $.ajax(settings).done(function (response) {
-	//console.log(response);
-	dataType: 'json';
-}).done(function(data){
-    console.log(data);
-        $.map(data, function(post, i) {
-        $('#packagepref').append('<h3>' + post.message + '</h3><p>' + post.body + '</p>');
-    });
-})
-
-*/
-
-/*-----------------Fetch and display packages to match search criteria using get HTML
-
-function fetchtype(){
+	console.log(response[0].result[8].hotel_name);
+});
     
-    var selectType = document.getElementById("holType").value;
-    var selectdest = document.getElementById("search_term").value;
-    console.log(selectdest);
     
-    $.get('index.html', function(search_term) {
-                $('#locpic').html(data);
-            });
-    
-    if (selectType == "famGet"){
-        $(document).ready(function(){
-            $.get('fampack.html', function(data) {
-                $('#packagepref').html(data);
+    }
+
+
+/*-----------------/booking apix------------------------------*/
+
+        /*
+        $.ajax({
+            method: 'GET',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            dataType: 'json'
+        }).done(function(data) {
+            console.log(data);
+            $.map(data, function(post, i) {
+                $('#packagepref').append('<h3>' + post.title + '</h3><p>' + post.body + '</p>');
             });
         });
-    } else if (selectType == "romGet"){
-        $(document).ready(function(){
-            $.get('rompack.html', function(data) {
-                $('#packagepref').html(data);
-            });
-        });
-    } else if (selectType == "friGet"){
-        $(document).ready(function(){
-            $.get('fripack.html', function(data) {
-                $('#packagepref').html(data);
-            });
-        });
-    }   
-}
 
-function locHead(){
-    var selectType = document.getElementById("search_term").value;
-}
-*/
+
+        $.ajax(settings).done(function (response) {
+        	//console.log(response);
+        	dataType: 'json';
+        }).done(function(data){
+            console.log(data);
+                $.map(data, function(post, i) {
+                $('#packagepref').append('<h3>' + post.message + '</h3><p>' + post.body + '</p>');
+            });
+        })
+
+        */
+
+        /*-----------------Fetch and display packages to match search criteria using get HTML
+
+        function fetchtype(){
+            
+            var selectType = document.getElementById("holType").value;
+            var selectdest = document.getElementById("search_term").value;
+            console.log(selectdest);
+            
+            $.get('index.html', function(search_term) {
+                        $('#locpic').html(data);
+                    });
+            
+            if (selectType == "famGet"){
+                $(document).ready(function(){
+                    $.get('fampack.html', function(data) {
+                        $('#packagepref').html(data);
+                    });
+                });
+            } else if (selectType == "romGet"){
+                $(document).ready(function(){
+                    $.get('rompack.html', function(data) {
+                        $('#packagepref').html(data);
+                    });
+                });
+            } else if (selectType == "friGet"){
+                $(document).ready(function(){
+                    $.get('fripack.html', function(data) {
+                        $('#packagepref').html(data);
+                    });
+                });
+            }   
+        }
+
+        function locHead(){
+            var selectType = document.getElementById("search_term").value;
+        }
+        */
